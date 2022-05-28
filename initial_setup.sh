7c1f5fc1c99b554024d1a832c801685df7fed29a
Md. Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-05-28 15:43:49 +0600

    Add initial_setup.sh

---
+++ initial_setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+read -r -p "Setup app (Y/N) : " SETUP
+if [ "$SETUP" != 'Y' ]; then
+    exit 1
+fi
+
+read -r -p "App name : " APP_NAME
+read -r -p "App ENV : " APP_ENV
+read -r -p "App Debug : " APP_DEBUG
+read -r -p "App URL : " APP_URL
+APP_URL="${APP_URL//\//\\/}"
+read -r -p "DB_DATABASE : " DB_DATABASE
+read -r -p "DB_USERNAME : " DB_USERNAME
+read -r -p "DB_PASSWORD : " DB_PASSWORD
+
+FILE_NAME='.env'
+
+cp .env.example $FILE_NAME
+
+sed -i "s/APP_NAME=.*/APP_NAME=\"$APP_NAME\"/" $FILE_NAME
+sed -i "s/APP_ENV=.*/APP_ENV=$APP_ENV/" $FILE_NAME
+sed -i "s/APP_DEBUG=.*/APP_DEBUG=$APP_DEBUG/" $FILE_NAME
+sed -i "s/APP_URL=.*/APP_URL=\"$APP_URL\"/" $FILE_NAME
+sed -i "s/DB_DATABASE=.*/DB_DATABASE=\"$DB_DATABASE\"/" $FILE_NAME
+sed -i "s/DB_USERNAME=.*/DB_USERNAME=\"$DB_USERNAME\"/" $FILE_NAME
+sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=\"$DB_PASSWORD\"/" $FILE_NAME
+
+curl https://raw.githubusercontent.com/HazzazBinFaiz/laravel-git-hooks/main/post-receive >.git/hooks/post-receive
+
+
+composer install -o --no-dev
+php artisan key:generate
+php artisan storage:link
+php artisan migrate --force --seed
+php artisan optimize
+php artisan up
+
