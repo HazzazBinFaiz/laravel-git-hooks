a0c37aab05ec1db9c78d60ef0bcfdd41d1b3dcf3
Md. Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-05-28 16:12:47 +0600

    Pass contents to tmp file

--- setup_runner.sh
+++ setup_runner.sh
@@ -2,8 +2,8 @@
 
 tmpfile=$(mktemp)
 
-curl -s https://raw.githubusercontent.com/HazzazBinFaiz/laravel-git-hooks/main/initial_setup.sh > tmpfile
+curl -s https://raw.githubusercontent.com/HazzazBinFaiz/laravel-git-hooks/main/initial_setup.sh > "$tmpfile"
 
-bash tmpfile
+bash "$tmpfile"
 
 rm "$tmpfile"
